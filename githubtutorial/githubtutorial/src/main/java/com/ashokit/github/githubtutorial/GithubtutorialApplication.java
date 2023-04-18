d1739c26b9cd788768320f522a012e062a1c7ec9
dwitiyahalder02 <dwitiyah@gmail.com>
2023-04-18 15:36:38 +0530

    maven-project-tutorial

---
+++ githubtutorial/githubtutorial/src/main/java/com/ashokit/github/githubtutorial/GithubtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit.github.githubtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubtutorialApplication.class, args);
+	}
+
+}
---
+++ githubtutorial/githubtutorial/src/test/java/com/ashokit/github/githubtutorial/GithubtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashokit.github.githubtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
